f4aa14c4a329b00d3e9191f88ae7ce767530b839
robo2020 <robo2020@gmail.com>
2025-06-10 11:22:54 +0300

    compse override

--- env.sh
+++ env.sh
@@ -1,5 +1,9 @@
+sudo sysctl -w net.ipv4.ipfrag_time=3
+sudo sysctl -w net.ipv4.ipfrag_high_thresh=134217728
+sudo sysctl -w net.core.rmem_max=2147483647
+
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file:///workspace/cyclonedds.xml
+export CYCLONEDDS_URI=file://$PWD/cyclonedds.xml
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 source aliases.sh
